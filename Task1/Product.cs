509b1195e60744196234fbb5b3d18751b3e37032
Yusufboy Atajanov <Yusufboy_Atajanov@epam.com>
2022-03-03 21:21:13 +0500

    debuggingHomeTask end

--- Task1/Product.cs
+++ Task1/Product.cs
@@ -11,5 +11,18 @@
         public string Name { get; set; }
 
         public double Price { get; set; }
+
+        public bool Equals(Product product)
+        {
+
+            if (product != null && this.Name == product.Name && this.Price == product.Price)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
     }
 }
